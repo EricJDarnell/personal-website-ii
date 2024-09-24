18c632eedd0c96501f6d6e8f79e44251432ce94e
EricJDarnell <eric.j.darnell@gmail.com>
2024-09-23 20:57:16 -0400

    javascript.js and success.js updated

--- javascript.js
+++ javascript.js
@@ -1,3 +1,12 @@
 let page = 0;
 console.log("Welcome to Eric's Page");
 console.log("Hello World!");
+
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+    submitButton.disabled = true
+    e.preventDefault()
+    window.location.href = window.location.origin + '/success.html'
+})
